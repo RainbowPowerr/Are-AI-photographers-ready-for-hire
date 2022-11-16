0eac70dedb7fadb6d49c5cebeb570341fc87adbf
Andrea <117260753+RainbowPowerr@users.noreply.github.com>
2022-11-16 14:15:55 +0100

    Add files via upload

---
+++ image_data_code.R
@@ -0,0 +1,48 @@
+setwd("~/Documents/Uppsala universitet/T8 22 HT/Examensarbete/R kod")
+
+# simulation test
+
+# a vector with the promt-names
+promt <- c("weather", "bankruptcy", "houseing")
+
+# sample n=1000 with euqal prob for the different prompts
+set.seed(96)
+prompt_sample <- sample(x = promt, size = 1000, replace = TRUE, 
+                        prob = c((1/3), (1/3), (1/3)) 
+)
+
+# sample n = 1000 for the other variables
+guidance <- round(runif(n = 1000, min = 0.5, max = 20), digits = 1) # rounded to one decimal
+
+xtra_prompt <-  rbinom(n = 1000, size = 1, prob = 0.5)
+
+guide_image <-  rbinom(n = 1000, size = 1, prob = 0.5)
+
+inf_steps_log_sample<- runif(n = 1000, min = log(50), max = log(250)) # the loged values are uniform dist
+
+inf_steps<- round(exp(inf_steps_log_sample), digits = 0) # raise to the power of e to remove the log
+# this makes lower values of inf_steps more probable (i.e we have a skewed distribution)
+# There is more info in the lower values that the higher values -> easier to extrapolate info from high values
+# round the numbers to closest integer.
+
+hist(exp(inf_steps_log_sample)) # visualise the skewed dist for inf_steps
+
+# save in a df
+df <- data.frame(prompt = prompt_sample,
+                 guidance = guidance,
+                 inf_steps = inf_steps,
+                 xtra_prompt = xtra_prompt,
+                 guide_image_bin = guide_image)
+
+# create a help variable that tells which (if any) guide image should be used
+df$image <- NA
+df$image[df$prompt == "weather" & df$guide_image_bin == 1] <- "forest"
+df$image[df$prompt == "bankruptcy" & df$guide_image_bin == 1] <- "boxes"
+df$image[df$prompt == "houseing" & df$guide_image_bin == 1] <- "coinstack"
+df$image[ df$guide_image_bin == 0] <- "none"
+
+#inf_steps<- round(runif(n = 100, min = 50, max = 250), digits = 0)
+
+# save df as a .csv
+write.csv(df, file = "image_data.csv")
+
